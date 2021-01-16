40ea000ea59b799a79d596d6080843a800ecb56c
Rehman <abdulrehman1@aol.com>
2021-01-16 08:06:24 +0500

    with some stuff

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+// import './App.css';
+// import Menu from './components/Menu';
+// import Body from './components/Body';
+// import About from "./components/About.1";
+// import Contact from "./components/Contact";
+// import Resume from "./components/Resume";
+// import {BrowserRouter, Route , Switch} from 'react-router-dom';
+
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <Menu />
+       <Switch>
+       <Route path="/" exact component = {Body} />
+       <Route path="/About" component = {About} />
+       <Route path="/Resume" component = {Resume} />
+       <Route path="/Contact" component = {Contact} />
+       </Switch>
+       {/* <Body /> */}
+       
     </div>
+    </BrowserRouter>
   );
 }
 
